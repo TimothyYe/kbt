00eee4690fd75f9e51cbd7fa4c7e5d91d91dad1e
Timothy <yexiaozhou2003@gmail.com>
2015-12-26 21:05:46 +0800

    Remove bcrypt

--- web/models/user.ex
+++ web/models/user.ex
@@ -26,15 +26,18 @@ defmodule ElixirChina.User do
 
   def valid_password?(record, password) do
     salt = String.slice(record.password, 0, 29)
-    {:ok, hashed_password} = :bcrypt.hashpw(password, salt)
-    "#{hashed_password}" == record.password
+    #{:ok, hashed_password} = :bcrypt.hashpw(password, salt)
+    #"#{hashed_password}" == record.password
+    password == record.password
+
   end
 
   def encrypt_password(password) do
     if password != nil do
       {:ok, salt} = :bcrypt.gen_salt
       {:ok, hashed_password} = :bcrypt.hashpw(password, salt)
-      hashed_password
+      #hashed_password
+      password
     end
   end
 end
