2db26be086d471e8834760e534160c40cd0be522
Timothy <yexiaozhou2003@gmail.com>
2016-02-18 16:27:47 +0800

    Update test cases

--- config/test.exs
+++ config/test.exs
@@ -2,4 +2,11 @@ use Mix.Config
 
 config :elixir_china, ElixirChina.Endpoint,
   http: [port: System.get_env("PORT") || 4001]
+
+config :elixir_china, ElixirChina.Repo,
+  adapter: Ecto.Adapters.MySQL,
+  database: "kbt",
+  username: "root",
+  password: "",
+  hostname: "localhost"
   
